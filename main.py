d629bb910dbb4ad7252e88f85fed81b9f548665b
vitaliy <anatoliy6791@mail.ru>
2022-05-17 21:19:32 +0300

    lab rab 2

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import requests
 s_city = "Moscow, RU"
 appid = "ea7d7d1154a9bd17d45f79deadfb0456"
-res = requests.get("http://api.openweathermap.org/data/2.5/weather",
-    params={'q': s_city, 'units': 'metric', 'lang': 'ru', 'APPID': appid})
+res = requests.get("http://api.openweathermap.org/data/2.5/forecast",
+    params={'q': s_city, 'units': 'metric', 'lang': 'ru','APPID': appid})
 data = res.json()
-print("Город:", s_city)
-print("Погодные условия:", data['weather'][0]['description'])
-print("Температура:", data['main']['temp'])
-print("Минимальная температура:", data['main']['temp_min'])
-print("Максимальная температура", data['main']['temp_max'])
+for i in data['list']:
+    print("Дата <", i['dt_txt'], "> \r\nСкорость ветра <", i['wind']['speed'])
+    print("Дата <", i['dt_txt'], "> \r\nВидимость <", i['visibility'])
+    print("____________________________")
+
+
