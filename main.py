1b6d90891fc3fe4a04924e9f39876dfd8bc0be95
vitaliy <anatoliy6791@mail.ru>
2022-05-17 20:34:39 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import requests
+s_city = "Moscow, RU"
+appid = "ea7d7d1154a9bd17d45f79deadfb0456"
+res = requests.get("http://api.openweathermap.org/data/2.5/weather?q=moscow&units=metric&lang=ru&appid=ea7d7d1154a9bd17d45f79deadfb0456")
+##params={'q': s_city, 'units': 'metric', 'lang': 'ru', 'APPID': appid}
+data = res.json()
+print("Город:", s_city)
+print("Погодные условия:", data['weather'][0]['description'])
+print("Температура:", data['main']['temp'])
+print("Минимальная температура:", data['main']['temp_min'])
+print("Максимальная температура", data['main']['temp_max'])
